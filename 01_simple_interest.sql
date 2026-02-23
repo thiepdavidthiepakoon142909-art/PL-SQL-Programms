9ed7a1e30a7405f276b1a4747ce269ff2479e800
thiepdavidthiepakoon142909-art <thiepdavidthiepakoon.142909@marwadiuniversity.ac.in>
2026-02-23 15:46:26 +0530

    Add files via upload

---
+++ 01_simple_interest.sql
@@ -0,0 +1,15 @@
+DECLARE
+    p NUMBER;
+    r NUMBER;
+    t NUMBER;
+    si NUMBER;
+BEGIN
+    p := &principal;
+    r := &rate;
+    t := &time;
+
+    si := (p * r * t) / 100;
+
+    DBMS_OUTPUT.PUT_LINE('Simple Interest = ' || si);
+END;
+/
---
+++ 02_square_cube.sql
@@ -0,0 +1,14 @@
+DECLARE
+    num NUMBER;
+    square NUMBER;
+    cube NUMBER;
+BEGIN
+    num := &number;
+
+    square := num * num;
+    cube := num * num * num;
+
+    DBMS_OUTPUT.PUT_LINE('Square = ' || square);
+    DBMS_OUTPUT.PUT_LINE('Cube = ' || cube);
+END;
+/
